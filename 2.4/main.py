10b3a9a8fdf9b80d0875c0ec1f78a9d9da66784e
Bella-Gu <1325276@learn.vsb.bc.ca>
2025-09-29 22:02:53 +0000

    hi

--- 2.4/main.py
+++ 2.4/main.py
@@ -1,6 +1,14 @@
 file = open("2.4/responses.csv")
 
+reply = input("what's your name?").lower().strip()
+
 for line in file:
-    if "bella" in line.lower():
+    if reply in line.lower():
+        print(line)
+        myline = line
+
+reply2 = input("who do you want to compare yourself to? enter a name in the comp sci class.").lower.strip()
+for line in file:
+    if reply2 in line.lower():
         print(line)
         myline = line
--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,3 +6,19 @@ You must use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
+
+file = open("2.4/responses.csv")
+
+reply = input("what's your name?").lower().strip()
+
+for line in file:
+    if reply in line.lower():
+        print(line)
+        myline = line
+
+reply2 = input("who do you want to compare yourself to? enter a name in the comp sci class.").lower().strip()
+for line in file:
+    if reply2 in line.lower():
+        print(line)
+        myline = line
+
