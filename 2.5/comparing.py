2d2d3b5cbc55f3994aa58c3991f5189e0d36e413
Bella-Gu <1325276@learn.vsb.bc.ca>
2025-10-05 07:12:49 +0000

    hi

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -7,38 +7,53 @@ You must design your algorithm in English first, then translate it to Python cod
 Test as you go! Describe in your comments what steps you took to test your code.
 """
 
+f"""
+Simple Personality Comparison Program
+Compares survey responses between two people
+"""
+
+# Bella Gu Data Analysis
+# 1) Introduction and ask for 2 user's name
+# 2) Compare the 2 users and return a number of things they have in common
+# 3) Return how similar they are based off the observation of how much they have in common
+
+# Read the data file
 file = open("2.4/responses.csv")
-filee = file.readlines()
-line = ""
-line2 = ""
+lines = file.readlines()
 
-reply = input("what's your name? ").lower().strip()
-reply2 = input("who do you want to compare yourself to? ").lower().strip()
+# Introduction
+print("\nWelcome to the compatibility score! \nEnter 2 names and find out how many things they have in common with eachother.")
 
-for line in filee:
-    if reply in line.lower():
-        print(line)
-        line = line
+# Get user input (2 names)
+name1 = input("\nName 1: ").lower().strip()
+name2 = input("name 2: ").lower().strip()
 
-    if reply2 in line.lower():
-        print(line)
-        line2 = line2
+# Find both people in the data
+person1 = ""
+person2 = ""
 
-line = line.split(",")
-line2 = line2.split(",")
-user = list(line)
+for line in lines:
+    if name1 in line.lower():
+        person1 = line.strip().split(",") # Split the data into a list
+    if name2 in line.lower():
+        person2 = line.strip().split(",") # Split the data into a list
 
+# Compare their answers (skip the name at index 0)
+matches = 0
+for match in range(1, min(len(person1), len(person2))):
+    if person1[match] == person2[match]:
+        matches += 1
 
-compare = [item for item in line if item in line2]
-result = len((compare)
+print("\nNumber of things in common: " + str(matches))
 
-if result <= 0:
-    print("You guys are not alike.")
-elif result <= 5:
-    print("You guys are somewhat alike.")
-elif result <= 9:
-    print("You guys are similar to eachother.")
-elif result >= 10:
-    print("You guys share everything in common.")
+# Determine compatibility level
+if matches <= 2:
+    print("You guys are not alike. Don't worry, opposites attract... Am I right..?")
+elif matches <= 4:
+    print("You guys are somewhat alike. Befriend them! Bring a smile on their face!")
+elif matches <= 6:
+    print("You guys are quite similar to eachother. Try talking to them! Who knows what friendship you guys will have?")
+elif matches >= 8:
+    print("You guys share a lot in common! It's best to start getting to know them better! You guys can be best friends forever!")
 else:
     print("I'm not sure I understand.")
