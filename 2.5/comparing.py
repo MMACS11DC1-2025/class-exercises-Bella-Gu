e9667961c8e1ca24121a78d1ec70afd8134d5d68
Bella-Gu <1325276@learn.vsb.bc.ca>
2025-10-08 21:47:29 +0000

    hi

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -12,11 +12,12 @@ Simple Personality Comparison Program
 Compares survey responses between two people
 """
 
-# Bella Gu Data Analysis
-# 1) Introduction and ask for 2 user's name
-# 2) Compare the 2 users and return a number of things they have in common
-# 3) Return how similar they are based off the observation of how much they have in common
-
+"""
+Bella Gu Data Analysis
+1) Introduction and ask for 2 user's name
+2) Compare the 2 users and return a number of things they have in common
+3) Return how similar they are based off how much they have in common
+"""
 # Open and read the data file
 file = open("2.4/responses.csv")
 lines = file.readlines()
@@ -38,27 +39,32 @@ for line in lines:
     if name2 in line.lower():
         person2 = line.strip().split(",") # Split the data into a list
 
-# Compare their answers (skip the name at index 0)
+# If name is not found in the database
+if person1 == "" or person2 == "":
+    print("Oops! Name(s) not found. Please try again.")
+    exit()
+
+# Compare their answers (skip the number and name at index 0)
 matches = 0
-for match in range(1, min(len(person1), len(person2))):
+for match in range(2, min(len(person1), len(person2))):
     if person1[match] == person2[match]:
         matches += 1
 
 print("\nNumber of things in common: " + str(matches))
 
 # Determine compatibility
-if matches <= 2:
-    print("You guys are not alike. Don't worry, opposites attract... Am I right..?")
-elif matches <= 4:
-    print("You guys are somewhat alike. Befriend them! Bring a smile on their face!")
-elif matches <= 6:
-    print("You guys are quite similar to eachother. Try talking to them! Who knows what friendship you guys will have?")
+if matches <= 1:
+    print("\nYou guys are not alike. It's okay, I'm sure you guys will be good friends.")
+elif matches > 1 and matches <= 3:
+    print("\nYou guys are somewhat alike. Befriend them! Bring a smile on their face!")
+elif matches > 3 and matches <= 6:
+    print("\nYou guys are quite similar to eachother. Try talking to them! Who knows what friendship you guys will have?")
 elif matches >= 8:
-    print("You guys share a lot in common! It's best to start getting to know them better! You guys can be best friends forever!")
-else:
-    print("I'm not sure I understand.")
+    print("\nYou guys share a lot in common! It's best to start getting to know them better! You guys can be best friends forever!")
 
-
-# Results:
-# 1) Compared Bella and Bella, prints correct comment
-# 2) Compared Bella and Erisha, prints correct comment
+"""
+Results:
+1) Compared Bella and Bella, prints correct comment (Share a lot matches in comon)
+2) Compared Bella and Erisha, prints correct comment (Somewhat alike)
+3) Compared 'Hello' and 'Goodbye', prints correct comment (Names not found)
+"""
