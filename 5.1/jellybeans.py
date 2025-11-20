b92548aecb7bc627303a492357977d9c6df3d53d
Bella-Gu <1325276@learn.vsb.bc.ca>
2025-11-20 21:03:17 +0000

    are you american

--- 5.1/jellybeans.py
+++ 5.1/jellybeans.py
@@ -1,17 +1,37 @@
 from PIL import Image
-import jellybeans
 
-jelly = Image.open("5.1/jelly_beans.jpg")
+def colour(r, g, b):
+    if r > 150 and g > 150 and b < 150:
+        return "yellow"
+    else:
+        return "other"
+   
+file = Image.open("5.1/jelly_beans.jpg")
+jb_image = file.load()
 
-width = image_output.width
-height = image_output.height
+print(jb_image[0,0])
+print(colour(jb_image[0,0][0], jb_image[0,0][1], jb_image[0,0][2]))
+
+yellow_pixels = []
+
+width = file.width
+height = file.height
 
 for x in range(width):
     for y in range(height):
-        r = jelly[x, y][0]
-        g = jelly[x, y][1]
-        b = jelly[x, y][2]
+        pixel_r = jb_image[x,y][0]
+        pixel_g = jb_image[x,y][1]
+        pixel_b = jb_image[x,y][2]
 
-        if coolcolours.colour(r, g, b) == "yellow":
-            image_output.putpixel((x, y), (255, 255, 255))
-image.output.save("output.png", "png")
+        if colour(pixel_r, pixel_g, pixel_b) == "yellow":
+            yellow_pixels.append(jb_image[x,y])
+            file.putpixel((x,y), (255, 255, 255))
+
+num_yellow = len(yellow_pixels)
+total_pixels = width*height
+file.save("output.png", "png")
+
+yellow_ratio = num_yellow / total_pixels
+
+print(len(yellow_pixels))
+print(width*height)
