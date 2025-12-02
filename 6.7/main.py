6528cf2546ff7522fb69d1e55b323e88bcb1c120
Bella-Gu <1325276@learn.vsb.bc.ca>
2025-12-02 07:16:31 +0000

    fixed file names and added lists for pixel colours

--- 6.7/main.py
+++ 6.7/main.py
@@ -9,33 +9,40 @@ t1 = time.time()
     # (e.g. colour channels as RGB tuple inputs) and returns a specific, useful output 
     # (e.g., returns True if the pixel matches your custom feature definition else False, or a weight)
 
-# load image file (import 10 photos)
-file1 = Image.open("6.7/filename.jpg")      # 10 images not imported yet
-file2 = Image.open("6.7/filename.jpg")
-file3 = Image.open("6.7/filename.jpg")
-file4 = Image.open("6.7/filename.jpg")
-file5 = Image.open("6.7/filename.jpg")
-file6 = Image.open("6.7/filename.jpg")
-file7 = Image.open("6.7/filename.jpg")
-file8 = Image.open("6.7/filename.jpg")
-file9 = Image.open("6.7/filename.jpg")
-file10 = Image.open("6.7/filename.jpg")
+# load image files (import 10 photos)
+file1 = Image.open("6.7/bread1.jpg")
+file2 = Image.open("6.7/bread2.jpg")
+file3 = Image.open("6.7/bread3.jpg")
+file4 = Image.open("6.7/bread4.jpg")
+file5 = Image.open("6.7/bread5.jpg")
+file6 = Image.open("6.7/bread6.jpg")
+file7 = Image.open("6.7/bread7.jpg")
+file8 = Image.open("6.7/bread8.jpg")
+file9 = Image.open("6.7/bread9.jpg")
+file10 = Image.open("6.7/bread10.jpg")
 
 # create a pixel access object for faster pixel manipulation
-name_image = file1.load()
-name_image = file2.load()
-name_image = file3.load()
-name_image = file4.load()
-name_image = file5.load()
-name_image = file6.load()
-name_image = file7.load()
-name_image = file8.load()
-name_image = file9.load()
-name_image = file10.load()
+bread1 = file1.load()
+bread2 = file2.load()
+bread3 = file3.load()
+bread4 = file4.load()
+bread5 = file5.load()
+bread6 = file6.load()
+bread7 = file7.load()
+bread8 = file8.load()
+bread9 = file9.load()
+bread10 = file10.load()
 
 # print the RGB values and colour category of the first pixel (top left corner)
 
 # create a list to store the pixels of the specific colour
+beige_pixels = []
+lightbrown_pixels = []
+mediumbrown_pixels = []
+darkbrown_pixels = []
+black_pixels = []
+other_pixels = [] # for pixels that don't match any category (eg. white background)
+
 t2 = time.time()
 # get image dimensions
 
