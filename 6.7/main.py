0630d76ea37f30e1cb8434b285bc03f828c43112
Bella-Gu <1325276@learn.vsb.bc.ca>
2025-12-07 01:30:19 +0000

    hi error

--- 6.7/main.py
+++ 6.7/main.py
@@ -10,8 +10,8 @@ t1 = time.time()
         # (e.g., returns True if the pixel matches your custom feature definition else False, or a weight)
 def is_target_feature(r, g, b):
     if r > 230 and g > 230 and b > 230:
-            return "white"                  # white background (edit)
-    if r > 235 and b > 214 and b > 183:
+            return "white"                  # white background
+    if r > 235 and g > 214 and b > 183:
         return "beige"
     elif r > 224 and g > 173 and b > 89:
         return "lightbrown"
@@ -39,15 +39,14 @@ black_pixels = []
 other_pixels = [] # for pixels that don't match any category (eg. white background)
 
 t2 = time.time()
-# get image dimensions thtrhrthhtr
 
 # iterate through every pixel in the image (go through all the pixels in the image)
 for i in range(len(breads)):
     width = file.width      # get image dimensions
     height = file.height
 
-    for x in range(height):
-        for y in range(width):
+    for x in range(width):
+        for y in range(height):
             pixel_r = (breads[i])[x,y][0]
             pixel_g = (breads[i])[x,y][1]
             pixel_b = (breads[i])[x,y][2]
@@ -79,7 +78,10 @@ for i in range(len(breads)):
     # calculate "Feature Density Score" for each image (percentage)
     total_burnt = num_mediumbrown + num_darkbrown + num_black # beige and light brown do not count because it is not burnt
     total_bread = num_beige + num_lightbrown + num_mediumbrown + num_darkbrown + num_black # total area of the bread
-    burnt_percentage = total_burnt / total_bread * 100
+    if total_bread > 0: # avoid dividing by 0 for safety check
+        burnt_percentage = total_burnt / total_bread * 100
+    else:
+        burnt_percentage = 0
 
     # print results
         # "based off the colours (medium, black...), your bread #1 is ...% burnt"
